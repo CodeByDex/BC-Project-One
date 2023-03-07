acae0f8980894edad99327ef8b2fa0f7709d254c
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-03-06 19:23:22 -0500

    added function for finding movie title

--- docs/assets/scripts/BookAPI.js
+++ docs/assets/scripts/BookAPI.js
@@ -1,5 +1,20 @@
+let movieAPILink = "http://www.omdbapi.com/?apikey=";
+let movieAPIKey = "ab6fe753";
+
 function FindBookByTitle(titleText){};
 function FindBookByTitle(titleText){};
 function GetBookWatchListFromLocalStorage(){};
 function AddBookToWatchList(Result){};
 function RemoveBookFromReadList(Result){};
+
+function findMovieByTitle(titleText, callback){
+    fetch(movieAPILink + movieAPIKey + "/?t=" + titleText)
+    .then(response => {
+        if(response.ok){
+            response.json()           
+        }
+    })
+    .then(data => {
+        callback(data);
+    })   
+}
