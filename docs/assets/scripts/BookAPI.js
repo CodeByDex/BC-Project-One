3ca26c5b6307f4228565f38c58873bb206fb18ea
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-03-10 20:38:38 -0500

    added validation that year value exists

--- docs/assets/scripts/BookAPI.js
+++ docs/assets/scripts/BookAPI.js
@@ -11,22 +11,21 @@ let movieAPIKey = "ab6fe753";
  * 
  *******************************************************/
 
-function FindBookByTitle(titleText, someFunction){
+function FindBookByTitle(titleText, someFunction) {
     FindBooksByTitle(titleText, (results) => {
         someFunction(results[0]);
     })
 };
-function FindBooksByTitle(titleText, someFunction){
+function FindBooksByTitle(titleText, someFunction) {
     let params = new URLSearchParams();
 
     params.append("q", titleText);
 
     let booksFound = [];
 
-    fetch("https://openlibrary.org/search.json?"+params.toString())
+    fetch("https://openlibrary.org/search.json?" + params.toString())
         .then(response => {
-            if(response.ok)
-            {
+            if (response.ok) {
                 return response.json();
             } else {
                 booksFound.push({
@@ -38,15 +37,19 @@ function FindBooksByTitle(titleText, someFunction){
             someFunction(booksFound);
         })
         .then(results => {
-            if (results.numFound > 0)
-            {
+            if (results.numFound > 0) {
                 results.docs.forEach(book => {
+                    let subtitle = "Published in " + book.first_publish_year;
+                    if (book.first_publish_year === undefined) {
+                        subtitle = "Publish Date Unknown"
+                    }
+
                     booksFound.push({
                         Type: "Book",
                         Title: book.title,
                         Author: book.author_name,
-                        Subtitle: "Released in " + book.first_publish_year,
-                        ImageURL: "https://covers.openlibrary.org/b/id/" + book.cover_i + "-L.jpg" 
+                        Subtitle: subtitle,
+                        ImageURL: "https://covers.openlibrary.org/b/id/" + book.cover_i + "-L.jpg"
                     });
                 });
             } else {
@@ -60,58 +63,56 @@ function FindBooksByTitle(titleText, someFunction){
         });
 };
 
-// FindBooksByTitle("The Hobbit", (results) => {
-//     console.log(results)
-// });
-
-function findMovieByTitle(titleText, callback){
+function findMovieByTitle(titleText, callback) {
     fetch(movieAPILink + movieAPIKey + "&t=" + titleText)
-    .then(response => {
-        if(response.ok){
-            return response.json()
-        } else {
-            return response.status;
-        }
-    })
-    .then(data => {
-        if(!data.Error){
-            callback(data);
-        } else {
-            callback(data.Error);
-        }
-    })   
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                return response.status;
+            }
+        })
+        .then(data => {
+            if (!data.Error) {
+                callback(data);
+            } else {
+                callback(data.Error);
+            }
+        })
 }
 
-function findMoviesByTitle(titleText, callback){
+function findMoviesByTitle(titleText, callback) {
     let foundMovies = [];
     fetch(movieAPILink + movieAPIKey + "&s=" + titleText)
-    .then(response => {
-        if(response.ok){
-            return response.json();
-        } else {
-            return response.status;
-        }
-    })
-    .then(data => {
-        if(!data.Error){
-            data.Search.forEach(movie => {
-                foundMovies.push({
-                    Type: "Movie",
-                    Title: movie.Title,
-                    Subtitle: "Released in " + movie.Year,
-                    ImageURL: movie.Poster
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return response.status;
+            }
+        })
+        .then(data => {
+            if (!data.Error) {
+                data.Search.forEach(movie => {
+                    let subtitle = "Released in " + movie.Year;
+                    if (movie.Year === undefined) {
+                        subtitle = "Unknown Release Date";
+                    }
+                    
+                    foundMovies.push({
+                        Type: "Movie",
+                        Title: movie.Title,
+                        Subtitle: subtitle,
+                        ImageURL: movie.Poster
+                    })
                 })
-            })
-            callback(foundMovies);
-        } else {
-            foundMovies.push({
-                Type: "Error",
+                callback(foundMovies);
+            } else {
+                foundMovies.push({
+                    Type: "Error",
                     ErrorMessage: data.Error
-            })
-            callback(foundMovies);
-        }
-    })
-}
-
-// findMoviesByTitle("Lord of the Rings", results => console.log(results));
-// findMovieByTitle("Lord of the Rings", results => console.log(results));
+                })
+                callback(foundMovies);
+            }
+        })
+};
