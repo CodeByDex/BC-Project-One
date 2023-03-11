66f7bb00c91bab4b921b6a1e43f7468965b60a61
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-03-11 14:52:42 -0500

    added check to search to see if an item is already saved

--- docs/assets/scripts/index.js
+++ docs/assets/scripts/index.js
@@ -53,64 +53,7 @@ function DisplayResults(results) {
 
         results.forEach(result => {
             if (result.Type != "Error") {
-                let newDivTile = document.createElement("div");
-                newDivTile.classList.add("tile")
-                newDivTile.classList.add("is-4")
-                newDivTile.classList.add("p-2")
-
-                let newCard = document.createElement("div");
-                newCard.classList.add("card")
-
-                let newCardImage = document.createElement("div");
-                newCardImage.classList.add("card-image")
-
-                let newFigure = document.createElement("figure");
-                newFigure.classList.add("img")
-                newFigure.classList.add("is-4by3")
-
-                let newImage = document.createElement("img");
-                newImage.setAttribute("src", result.ImageURL)
-                newImage.setAttribute("alt", "Movie Title Alt")
-
-                let newDivContent = document.createElement("div");
-                newDivContent.classList.add("card-content")
-                newDivContent.classList.add("p-3")
-
-                let newContentTitle = document.createElement("h3")
-                newContentTitle.classList.add("title")
-                newContentTitle.classList.add("is-4")
-                newContentTitle.textContent = result.Title;
-
-                let newContentSubtitle = document.createElement("h4");
-                newContentSubtitle.classList.add("subtitle");
-                newContentSubtitle.classList.add("is-5");
-                newContentSubtitle.textContent = result.Subtitle;
-
-                let newCardFooter = document.createElement("div");
-                newCardFooter.classList.add("card-footer");
-
-                let newAddListButton = document.createElement("a");
-                newAddListButton.classList.add("add-favorite"); // Check class type
-                newAddListButton.classList.add("card-footer-item");
-                newAddListButton.classList.add("button");
-                newAddListButton.classList.add("is-fullwidth");
-                newAddListButton.textContent = "Add to my List";
-                newAddListButton.dataset.Type = result.Type;
-                newAddListButton.dataset.Mode = "Add";
-                newAddListButton.addEventListener("click", clickAddButton)
-
-
-                newDivTile.appendChild(newCard);
-                newCard.appendChild(newCardImage);
-                newCardImage.appendChild(newFigure);
-                newFigure.appendChild(newImage);
-                newCard.appendChild(newDivContent);
-                newDivContent.appendChild(newContentTitle);
-                newDivContent.appendChild(newContentSubtitle);
-                newDivContent.appendChild(newCardFooter);
-                newCardFooter.appendChild(newAddListButton);
-                resultsEL.appendChild(newDivTile);
-
+                CreateResultCard(result);
             } else {
                 let pEL = document.createElement("p");
                 pEL.textContent = result.ErrorMessage;
@@ -129,6 +72,83 @@ function DisplayResults(results) {
     searchButton.removeAttribute("disabled");
 
 };
+
+async function CreateResultCard(result) {
+    let newDivTile = document.createElement("div");
+    newDivTile.classList.add("tile");
+    newDivTile.classList.add("is-4");
+    newDivTile.classList.add("p-2");
+
+    let newCard = document.createElement("div");
+    newCard.classList.add("card");
+
+    let newCardImage = document.createElement("div");
+    newCardImage.classList.add("card-image");
+
+    let newFigure = document.createElement("figure");
+    newFigure.classList.add("img");
+    newFigure.classList.add("is-4by3");
+
+    let newImage = document.createElement("img");
+    newImage.setAttribute("src", result.ImageURL);
+    newImage.setAttribute("alt", "Movie Title Alt");
+
+    let newDivContent = document.createElement("div");
+    newDivContent.classList.add("card-content");
+    newDivContent.classList.add("p-3");
+
+    let newContentTitle = document.createElement("h3");
+    newContentTitle.classList.add("title");
+    newContentTitle.classList.add("is-4");
+    newContentTitle.textContent = result.Title;
+
+    let newContentSubtitle = document.createElement("h4");
+    newContentSubtitle.classList.add("subtitle");
+    newContentSubtitle.classList.add("is-5");
+    newContentSubtitle.textContent = result.Subtitle;
+
+    let newCardFooter = document.createElement("div");
+    newCardFooter.classList.add("card-footer");
+
+    let newAddListButton = document.createElement("a");
+    newAddListButton.classList.add("add-favorite"); // Check class type
+    newAddListButton.classList.add("card-footer-item");
+    newAddListButton.classList.add("button");
+    newAddListButton.classList.add("is-fullwidth");
+    newAddListButton.dataset.Type = result.Type;
+
+    let existingList;
+
+    if(result.Type === "Movie")
+    {
+        existingList = getMoviesFromLocalStorage();
+    } else {
+        existingList = getBooksFromLocalStorage();
+    }
+
+    if (existingList.findIndex(x => x.Title === result.Title) >= 0)
+    {
+        newAddListButton.textContent = "Remove from List";
+        newAddListButton.dataset.Mode = "Remove";
+        newAddListButton.addEventListener("click", clickAddButton);
+
+    } else {
+        newAddListButton.textContent = "Add to my List";
+        newAddListButton.dataset.Mode = "Add";
+        newAddListButton.addEventListener("click", clickAddButton);
+    }
+
+    newDivTile.appendChild(newCard);
+    newCard.appendChild(newCardImage);
+    newCardImage.appendChild(newFigure);
+    newFigure.appendChild(newImage);
+    newCard.appendChild(newDivContent);
+    newDivContent.appendChild(newContentTitle);
+    newDivContent.appendChild(newContentSubtitle);
+    newDivContent.appendChild(newCardFooter);
+    newCardFooter.appendChild(newAddListButton);
+    resultsEL.appendChild(newDivTile);
+}
 
 function clickAddButton(event) {
     let clickedCard = event.target.parentNode.parentNode.parentNode;
